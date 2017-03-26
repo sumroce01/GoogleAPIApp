245673ed5d6d328838dd68b4fea89e50b2d78b03
quineghost <quineghost@gmail.com>
2017-03-26 15:51:49 +0900

    test

---
+++ app/src/main/java/jp/co/roce/googleapiapp/MainActivity.java
@@ -0,0 +1,28 @@
+package jp.co.roce.googleapiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
